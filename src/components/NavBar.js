1a99d2a50e151fe0dc0da6c95802195062145e7d
Leonardo Kellner <leonardo.kellner@gmail.com>
2022-05-25 13:31:14 -0300

    agrego carpeta components y NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import logo from './logo.png'
+import './NavBar.css'
+
+export default function NavBar(){
+    return (
+        <header id='top'>
+            <a className="brand" href="#top"><img src={logo} alt="LOGO"></img></a>
+            <section className="search">
+                <input type="text" placeholder="Buscar..."></input>
+                <button>Buscar</button>
+            </section>
+            <nav>
+                <ul className="nav">
+                    <li><button>PRODUCTOS</button></li>
+                    <li><button>QUIENES SOMOS</button></li>
+                    <li><button>CONTACTO</button></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}

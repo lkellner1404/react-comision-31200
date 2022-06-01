14bfde0bc9df5ec3ee5af95870fec55691abdc34
Leonardo Kellner <leonardo.kellner@gmail.com>
2022-06-01 13:58:32 -0300

    agrego logica de stock y estilo a ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,25 +1,28 @@
 import { useState } from "react"
+import './style.css'
 
 export default function ItemCount( {stock, initial, onAdd} ) {
     const number = Number(initial)
     const [count, setcount] = useState(number)
     const agregar =()=>{
-        setcount(count + 1)
+        if (stock > count){
+            setcount(count + 1)
+        }
     }
     const quitar =()=>{
-        setcount(count - 1)
+        if (count > 0){
+            setcount(count - 1)
+        }
     }
 
     return(
-        <>
-        <section>
-            <button onClick={quitar}>-</button>
-            <span>{count}</span>
-            <button onClick={agregar}>+</button>
-        </section>
-        <section>
+        <section className="item-count">
+            <section className="contador">
+                <button onClick={quitar}>-</button>
+                <span>{count}</span>
+                <button onClick={agregar}>+</button>
+            </section>
             <button>Agregar al Carrito</button>
         </section>
-        </>
     )
 }

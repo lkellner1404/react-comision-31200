32196e778bed7bfbdc9176da8d83a043475e31e2
Leonardo Kellner <leonardo.kellner@gmail.com>
2022-06-30 16:25:38 -0300

    linkeo firestore para carga de ordenes de compra

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/cartContext'
 // import CartItem from '../CartItem/CartItem'
 
  export default function Carrito(){
     const { cart, total, clearCart, removeItem } = useCartContext()
+    const [ ordenId, setOrdenId ] = useState('')
     console.log(cart)
+
+    if (ordenId){
+        return(
+            <section className='carrito'>
+                <h2>Carrito de Compras</h2>
+                <p>Su orden se generó con el ID: {ordenId}</p>
+                <p>Muchas gracias por elegirnos!</p>
+            </section>
+        )
+    }
 
     if (cart.length === 0){
         return (
             <section className='carrito'>
                 <h2>Carrito de Compras</h2>
                 <p>El carrito se encuentra vacío, para agregar productos haga <Link to='/'>CLICK AQUÍ</Link> </p>
-                
             </section>
         )
+    }
+
+    const comprar = () =>{
+        const items = cart.map((items)=>{
+            const id = items.id
+            const title = items.title
+            const price = items.price
+            const cantidad = items.cantidad
+            const totalProd = price * cantidad
+            return {id:id, title:title, price:price, cantidad:cantidad, total: totalProd}
+        })
+        const orden =
+            {
+                buyer: {name:'AAAAA',phone:'111111',email:'aaaa@aaaa'},
+                items: items,
+                total: total,
+                date: new Date()
+            }
+        
+        const db = getFirestore()
+        const ordenes = collection(db,'orders')
+        addDoc(ordenes, orden)
+            .then(( { id } ) => setOrdenId(id))
+            .finally(()=>clearCart())
+        console.log(ordenId)
     }
     
     
@@ -30,10 +66,14 @@ import { useCartContext } from '../../context/cartContext'
                     }
                 </ul>
                 <button onClick={clearCart}>Vaciar Carrito</button>
+                <button onClick={comprar}>COMPRAR</button>
             </div>
             <div className='carrito--total'>
                 <h3>TOTAL $ {total}</h3>
             </div>
+            {
+                ordenId && <p>Su orden se generó con el ID: {ordenId}</p>
+            }
         </section>
     )
 }
